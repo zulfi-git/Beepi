d71fa649fb3ff6a4b4f76610caf8df7c7f3f8d6b
zulfi-git <zhaiden@gmail.com>
2025-02-28 04:38:46 +0100

    Bulk kjennemerke oppdatering

--- includes/class-svv-api-integration.php
+++ includes/class-svv-api-integration.php
@@ -427,7 +427,7 @@ class SVV_API_Integration {
             
             // Try with GET endpoint if available
             error_log("🔄 Trying direct endpoint with new token");
-            $direct_endpoint = $this->svv_api_base_url . '/kjoretoyoppslag/kjennemerke/' . urlencode($registration_number);
+            $direct_endpoint = $this->svv_api_base_url . '/kjoretoyoppslag/bulk/kjennemerke/' . urlencode($registration_number);
             error_log("🔄 Direct endpoint URL: " . $direct_endpoint);
             
             $response = wp_remote_get($direct_endpoint, [
